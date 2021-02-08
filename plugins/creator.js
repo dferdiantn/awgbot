252bd695e015ca315a840c2065b47bc448cc0c97
Dwi Ferdiantono <55092616+dferdiantn@users.noreply.github.com>
2021-02-08 17:35:14 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 let handler = function (m) {
   // this.sendContact(m.chat, '6282123618826', 'AWGcorp Admin', m)
-  this.sendContact(m.chat, '6282123618826', 'Admin AWGcorp', m)
+  this.sendContact(m.chat, '6282123618826', '𝑺̲̅𝑨̲̅𝑯̲̅𝑨̲̅ 𝑰̲̅𝑬̲̅𝑼̲̅', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
